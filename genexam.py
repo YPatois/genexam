0bd1d48abde3137743cbda8575ea41f1a95fc64d
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-04-07 08:52:23 +0200

    Stable random

--- genexam.py
+++ genexam.py
@@ -61,6 +61,7 @@ def generates_student_file(e):
 # Welcome to Derry, Maine
 # --------------------------------------------------------------------------
 def main():
+    random.seed(10)
     lc=LesClasses(lesclasses,None,False)
     cid="4_4"
     a_class=lc.getClasse(cid)
