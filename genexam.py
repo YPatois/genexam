500e7bf5d9b47d5db871f917c128b57591688482
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-04-07 11:33:20 +0200

    Starts to put levels

--- genexam.py
+++ genexam.py
@@ -30,16 +30,34 @@ def replace_braces(s):
     s=s.replace('£','}')
     return s
 
-def build_l(idx):
+def build_lmA(idx):
     clist=[ 10*i for i in range(1,10)]
     i=random.choice(clist)
     s="$@i_{}=\SI@{}£@\mA££$".format(idx,i)
     s=replace_braces(s)
     return (i,s)
 
-def problem_stuffing():
-    (i2,s2)=build_l(2)
-    (i3,s3)=build_l(3)
+def build_lA(idx):
+    clist=[ 10+2*i for i in range(1,9)]
+    i=random.choice(clist)/10
+    #print(i)
+    s="$@i_{}=\SI@{}£@\A££$".format(idx,i)
+    s=replace_braces(s)
+    return (i,s)
+
+def problem_stuffing_basic():
+    (i2,s2)=build_lmA(2)
+    (i3,s3)=build_lmA(3)
+    i1=i2+i3
+    ia="${i_1}$"
+    ib=s2
+    ic=s3
+    solstring=get_random_string(5)+str(i1)+get_random_string(5)
+    return (ia,ib,ic,solstring)
+
+def problem_stuffing_higher():
+    (i2,s2)=build_lmA(2)
+    (i3,s3)=build_lA(3)
     i1=i2+i3
     ia="${i_1}$"
     ib=s2
@@ -48,11 +66,17 @@ def problem_stuffing():
     return (ia,ib,ic,solstring)
 
 
+def problem_stuffing(level):
+    if (level<11):
+        return problem_stuffing_basic()
+    else:
+        return problem_stuffing_higher()
+
 def generates_student_file(e):
     cl=e.cid.split('_')
     fi=open("latextemplate.tex","rt")
     fo=open(eleve2filename(e),"wt")
-    (ia,ib,ic,sl)=problem_stuffing()
+    (ia,ib,ic,sl)=problem_stuffing(e.phynote)
     for line in fi.readlines():
         line=line.replace("@PRENOM@",e.prenom)
         line=line.replace("@NOM@",e.nom)
@@ -72,7 +96,7 @@ def generates_student_file(e):
 # --------------------------------------------------------------------------
 def main():
     random.seed(10)
-    lc=LesClasses(lesclasses,None,False)
+    lc=LesClasses(lesclasses,False)
     cid="4_4"
     a_class=lc.getClasse(cid)
     for e in a_class.eleves:
